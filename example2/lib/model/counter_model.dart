4d972c879f9aecd19196cd3d422f258e1e8e3680
samuelson <samuelsonowei04@gmail.com>
2023-03-01 00:50:23 +0100

    added the extension on InfixOperator (+, -)

--- example2/lib/model/counter_model.dart
+++ example2/lib/model/counter_model.dart
@@ -1,2 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+extension OptionalInfixAddition<T extends num> on T? {
+  T? operator +(T? other) {
+    final shadow = this;
+    if (shadow != null) {
+      shadow + (other ?? 0) as T;
+    } else {
+      return null;
+    }
+  }
+}
+
+extension OptionalInfixSubtraction<T extends num> on T? {
+  T? operator -(T? other) {
+    final shadow = this;
+    if (shadow != null) {
+      shadow - (other ?? 0) as T;
+    } else {
+      return null;
+    }
+  }
+}
