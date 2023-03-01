7764b97ae60e81cc79c71e0a3edcbaeb86932ddc
samuelson <samuelsonowei04@gmail.com>
2023-03-01 01:19:50 +0100

    finished the counter app

--- example2/lib/pages/home_page.dart
+++ example2/lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:example2/providers/counter_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -16,14 +17,34 @@ class _HomePageState extends ConsumerState<HomePage> {
         centerTitle: true,
         title: const Text('Example2 (Counter app)'),
       ),
-      body: const Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Consumer(
+            builder: (context, ref, child) {
+              final count = ref.watch(counterProvider);
+
+              return Text(
+                count == null ? 'click a button' : count.toString(),
+                style:
+                    const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              );
+            },
           ),
-        ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                onPressed: ref.read(counterProvider.notifier).increment,
+                child: const Icon(Icons.add),
+              ),
+              FloatingActionButton(
+                onPressed: ref.read(counterProvider.notifier).decrement,
+                child: const Icon(Icons.remove),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
