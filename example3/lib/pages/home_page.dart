4d17d0b16cab98946e233fc808256e1fd64b8cca
samuelson <samuelsonowei04@gmail.com>
2023-03-01 02:28:54 +0100

    commited

--- example3/lib/pages/home_page.dart
+++ example3/lib/pages/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:example3/models/city_model.dart';
+import 'package:example3/providers/city_provider.dart';
+import 'package:example3/providers/weather_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -11,10 +14,39 @@ class HomePage extends ConsumerStatefulWidget {
 class _HomePageState extends ConsumerState<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final currentWeather = ref.watch(weatherProvider);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Example3 (Weather app)'),
+      ),
+      body: Column(
+        children: [
+          currentWeather.when(
+            data: (weather) => Text(
+              weather,
+              style: const TextStyle(fontSize: 100),
+            ),
+            loading: () => const CircularProgressIndicator(),
+            error: (error, stack) => Text(error.toString()),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: City.values.length,
+              itemBuilder: (context, index) {
+                final city = City.values[index];
+                final isSelected = city == ref.watch(currentCityProvider);
+                return ListTile(
+                  title: Text(city.toString().split('.').last),
+                  trailing: isSelected ? const Icon(Icons.check) : null,
+                  onTap: () {
+                    ref.read(currentCityProvider.notifier).state = city;
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
